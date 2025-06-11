6cff187a9e4159f0ea3edc06f92f27115f28e7d8
jerryvoldofebian <voldojerry@gmail.com>
2025-06-11 11:27:36 +0700

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -31,7 +31,7 @@ const Index = () => {
       description: "Aplikasi ini digunakan dalam menerima permintaan konsultasi tatap muka oleh pelaku usaha dan sekaligus untuk mencatat hasil konsultasinya yang dapat diakses setiap saat. Didesain untuk tiga roles yaitu admin, konsultan dan klien. Setiap role dapat melakukan login dan mempunyai dashboard masing-masing",
       status: "Completed",
       tech: ["ReactJS", "Golang", "PostgreSQL"],
-      image: "https://photos.app.goo.gl/AvwkeSYg7vWkKAN1A",
+      image: "https://photos.app.goo.gl/RdCrXtng8L1rXQYz6",
       completion: 100
     },
     {
